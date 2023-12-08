7c300bb9c30aba8edacd3dc44c26e2a85a5b6b2b
pavellsan8 <pavelryan146@gmail.com>
2023-12-08 14:20:56 +0700

    login (msi kurang bener)

--- MMS_LecProject/LoginViewController.swift
+++ MMS_LecProject/LoginViewController.swift
@@ -1,14 +1,30 @@
-//
-//  LoginViewController.swift
-//  MMS_LecProject
-//
-//  Created by prk on 07/12/23.
-//
-
 import UIKit
+import CoreData
 
 class LoginViewController: UIViewController {
+    
+    var userData: [userdata] = []
+    var context: NSManagedObjectContext!
 
+    @IBOutlet weak var emailFieldLogin: UITextField!
+    @IBOutlet weak var passwordFieldLogin: UITextField!
+    @IBAction func loginBtn(_ sender: Any) {
+        userFetchData()
+        
+        let email = emailFieldLogin.text
+        let password = passwordFieldLogin.text
+        
+        if email!.isEmpty || password!.isEmpty {
+            alertAction(title: "Warning", message: "Field must be filled")
+        }
+        
+        for user in userData {
+            if (user.email != email || user.password != password){
+                alertAction(title: "Invalid Credential", message: "Email or Password is wrong")
+            }
+        }
+    }
+    
     @IBAction func moveToRegister(_ sender: Any) {
         if let nextView = storyboard?.instantiateViewController(identifier: "registerView") {
             let registerView = nextView as! RegisterViewController
@@ -18,6 +34,32 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
 
+        context = appDelegate.persistentContainer.viewContext
+    }
+    
+    func userFetchData(){
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        do{
+            let results = try context.fetch(request) as! [NSManagedObject]
+            for data in results{
+                userData.append(userdata(
+                    name: data.value(forKey: "name") as! String,
+                    email: data.value(forKey: "email") as! String,
+                    password: data.value(forKey: "password") as! String))
+            }
+            print("fetching successful")
+        }catch{
+            print("fetching failed")
+        }
+    }
+        
+        
+    func alertAction(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
     }
 }
---
+++ MMS_LecProject/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  MMS_LecProject
+//
+//  Created by prk on 08/12/23.
+//
+
+import Foundation
+
+struct userdata {
+    var name: String
+    var email: String
+    var password: String
+}
